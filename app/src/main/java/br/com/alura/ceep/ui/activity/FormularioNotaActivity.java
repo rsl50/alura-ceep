19604ce89f7a3da0c36ed3b24bae77a4b3f415df
Robson Lima <rsl.robson@gmail.com>
2020-02-10 16:57:58 -0300

    Adicionando formulário para criar notas

---
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
@@ -0,0 +1,15 @@
+package br.com.alura.ceep.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import br.com.alura.ceep.R;
+
+import android.os.Bundle;
+
+public class FormularioNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_nota);
+    }
+}
--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,6 +1,9 @@
 package br.com.alura.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -19,6 +22,15 @@ public class ListaNotasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_notas);
         List<Nota> todasNotas = notasDeExemplo();
         configuraRecyclerView(todasNotas);
+
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iniciaFormularioNota = new Intent(ListaNotasActivity.this, FormularioNotaActivity.class);
+                startActivity(iniciaFormularioNota);
+            }
+        });
     }
 
     private void configuraRecyclerView(List<Nota> todasNotas) {
