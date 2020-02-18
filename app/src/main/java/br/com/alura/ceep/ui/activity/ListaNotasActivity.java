ddfa94168bcdeec935b867267e3cb702e9acfd4f
Robson Lima <rsl.robson@gmail.com>
2020-02-18 09:51:29 -0300

    Aplicando o padrão de comunicação entre as Activities; Adicionada verificação para evitar NullPointer em temNota de ListaNotasActivity

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,5 +1,6 @@
 package br.com.alura.ceep.ui.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +23,6 @@ import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CHAVE_NOTA;
 import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CHAVE_POSICAO;
 import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CODIGO_REQUISICAO_ALTERA_NOTA;
 import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CODIGO_REQUISICAO_INSERE_NOTA;
-import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CODIGO_RESULTADO_NOTA_CRIADA;
 import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.POSICAO_INVALIDA;
 
 public class ListaNotasActivity extends AppCompatActivity {
@@ -64,20 +64,26 @@ public class ListaNotasActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (ehResultadoInsereNota(requestCode, resultCode, data)) {
-            //Nota notaRecebida = (Nota) data.getSerializableExtra(CHAVE_NOTA);
-            Nota notaRecebida = (Nota) data.getExtras().getParcelable(CHAVE_NOTA);
-            adiciona(notaRecebida);
+        if (ehResultadoInsereNota(requestCode, data)) {
+            if (resultadoOk(resultCode)) {
+                //Nota notaRecebida = (Nota) data.getSerializableExtra(CHAVE_NOTA);
+                Nota notaRecebida = (Nota) data.getExtras().getParcelable(CHAVE_NOTA);
+                adiciona(notaRecebida);
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Ocorreu um problema na inclusão da nota", Toast.LENGTH_SHORT).show();
+            }
         }
 
-        if (ehResultadoAlteraNota(requestCode, resultCode, data)) {
-            Nota notaRecebida = (Nota) data.getExtras().getParcelable(CHAVE_NOTA);
-            int posicaoRecebida = data.getIntExtra(CHAVE_POSICAO, POSICAO_INVALIDA);
+        if (ehResultadoAlteraNota(requestCode, data)) {
+            if (resultadoOk(resultCode)) {
+                Nota notaRecebida = (Nota) data.getExtras().getParcelable(CHAVE_NOTA);
+                int posicaoRecebida = data.getIntExtra(CHAVE_POSICAO, POSICAO_INVALIDA);
 
-            if (ehPosicaoValida(posicaoRecebida)) {
-                altera(notaRecebida, posicaoRecebida);
-            } else {
-                Toast.makeText(this, "Ocorreu um problema na alteração da nota", Toast.LENGTH_SHORT).show();
+                if (ehPosicaoValida(posicaoRecebida)) {
+                    altera(notaRecebida, posicaoRecebida);
+                } else {
+                    Toast.makeText(this, "Ocorreu um problema na alteração da nota", Toast.LENGTH_SHORT).show();
+                }
             }
         }
 
@@ -98,24 +104,24 @@ public class ListaNotasActivity extends AppCompatActivity {
         return posicaoRecebida > POSICAO_INVALIDA;
     }
 
-    private boolean ehResultadoAlteraNota(int requestCode, int resultCode, @Nullable Intent data) {
-        return ehCodigoRequisicaoAlteraNota(requestCode) && ehCodigoResultadoNotaCriada(resultCode) && temNota(data);
+    private boolean ehResultadoAlteraNota(int requestCode, @Nullable Intent data) {
+        return ehCodigoRequisicaoAlteraNota(requestCode) && temNota(data);
     }
 
     private boolean ehCodigoRequisicaoAlteraNota(int requestCode) {
         return requestCode == CODIGO_REQUISICAO_ALTERA_NOTA;
     }
 
-    private boolean ehResultadoInsereNota(int requestCode, int resultCode, @Nullable Intent data) {
-        return ehCodigoRequisicaoInsereNota(requestCode) && ehCodigoResultadoNotaCriada(resultCode) && temNota(data);
+    private boolean ehResultadoInsereNota(int requestCode, @Nullable Intent data) {
+        return ehCodigoRequisicaoInsereNota(requestCode) && temNota(data);
     }
 
     private boolean temNota(@Nullable Intent data) {
-        return data.hasExtra(CHAVE_NOTA);
+        return data != null ? data.hasExtra(CHAVE_NOTA) : false;
     }
 
-    private boolean ehCodigoResultadoNotaCriada(int resultCode) {
-        return resultCode == CODIGO_RESULTADO_NOTA_CRIADA;
+    private boolean resultadoOk(int resultCode) {
+        return resultCode == Activity.RESULT_OK;
     }
 
     private boolean ehCodigoRequisicaoInsereNota(int requestCode) {
--- app/src/main/java/br/com/alura/ceep/ui/activity/NotaActivityConstantes.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/NotaActivityConstantes.java
@@ -4,8 +4,6 @@ public interface NotaActivityConstantes {
     int CODIGO_REQUISICAO_INSERE_NOTA = 1;
     int CODIGO_REQUISICAO_ALTERA_NOTA = 2;
 
-    int CODIGO_RESULTADO_NOTA_CRIADA = 2;
-
     int POSICAO_INVALIDA = -1;
 
     String CHAVE_NOTA = "nota";
